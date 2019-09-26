a8f309332d970a2efcc6bcf869dcf816759a011b
yhlcm <yahelicm27@gmail.com>
2019-09-26 14:44:14 -0500

    Archivo actividad 1, suma de numeros

---
+++ public/app.js
@@ -0,0 +1,6 @@
+function Sumar() {
+    var n1 = document.getElementById('numero1').value;
+    var n2 = document.getElementById('numero2').value;
+    var suma = parseInt(n1) + parseInt(n2);
+    alert("La suma es: "+suma)
+}
